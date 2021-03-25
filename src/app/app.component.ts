ddb08a7e29ab38f31b012b382d69f793dbc78568
AnatolyOsovitny <anatoly.osovitny@outlook.com>
2021-03-26 03:05:33 +0700

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ export class AppComponent  {
     {id: 1, name: 'Python'},
     {id: 2, name: 'Node Js'},
     {id: 3, name: 'Java'},
-    {id: 4, name: 'PHP'},
+    {id: 4, name: 'PHP', disabled: true},
     {id: 5, name: 'Django'},
     {id: 6, name: 'Angular'},
     {id: 7, name: 'Vue'},
@@ -23,4 +23,16 @@ export class AppComponent  {
     {id: 2, name: 'Node Js'},
     {id: 8, name: 'ReactJs'}
   ];
+
+   items2 = [
+    {id: 1, name: 'Python', image: 'python.jpg'},
+    {id: 2, name: 'Node Js', image: 'nodejs.jpg'},
+    {id: 3, name: 'Java', image: 'java.jpg'},
+    {id: 4, name: 'PHP', image: 'php.jpg', disabled: true},
+    {id: 5, name: 'Django', image: 'django.jpg'},
+    {id: 6, name: 'Angular', image: 'angular.jpg'},
+    {id: 7, name: 'Vue', image: 'vue.jpg'},
+    {id: 8, name: 'ReactJs', image: 'reactjs.jpg'},
+  ];
+  selected2 = [ 2, 8 ];
 }
